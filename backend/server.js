508974b1f9f92e5f1f0b2cc417c904613cc6cea6
Ubaid <ubaidrazashaikh88@gmail.com>
2026-09-12 20:38:15 +0500

    Inatail commit

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,55 @@
+// // const express = require('express');
+// // const cors = require('cors');
+// // const dotenv = require('dotenv');
+
+// // const connectDB = require('./config/db');
+// // const healthRoutes = require('./routes/health');
+// // const categoriesRoutes = require('./routes/categories');
+// // const productsRoutes = require('./routes/products');
+
+// // dotenv.config();
+
+// // const app = express();
+// // const PORT = process.env.PORT || 5000;
+
+// // connectDB();
+
+// // app.use(cors());
+// // app.use(express.json());
+
+// // app.use(healthRoutes);
+// // app.use(categoriesRoutes);
+// // app.use(productsRoutes);
+
+// // app.listen(PORT, () => {
+// //   console.log(`Server running at http://localhost:${PORT}`);
+// // });
+
+// const express = require('express');
+// const cors = require('cors');
+// const dotenv = require('dotenv');
+
+// const connectDB = require('./config/db');
+// const healthRoutes = require('./routes/health');
+// const categoriesRoutes = require('./routes/categories');
+// const productsRoutes = require('./routes/products');
+
+// dotenv.config();
+
+// const app = express();
+
+// connectDB();
+
+// app.use(cors());
+// app.use(express.json());
+
+// app.use(healthRoutes);
+// app.use(categoriesRoutes);
+// app.use(productsRoutes);
+
+// module.exports = app;
+
+
 const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
@@ -10,9 +62,6 @@ const productsRoutes = require('./routes/products');
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
-
-connectDB();
 
 app.use(cors());
 app.use(express.json());
@@ -21,6 +70,6 @@ app.use(healthRoutes);
 app.use(categoriesRoutes);
 app.use(productsRoutes);
 
-app.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}`);
-});
+connectDB();
+
+module.exports = app;
